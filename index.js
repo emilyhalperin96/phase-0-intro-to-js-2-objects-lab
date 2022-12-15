c651ad94cb9124cb79db0776369c574c4ca72ee5
emilyphalperin@gmail.com <emilyhalperin@Emilys-MBP.lan>
2022-12-15 18:49:01 -0500

    commit tree 2f1e770f78a166a478d930f786b9b5801cb338bd

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+const employee = {}
+function updateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+   const newList = {...employee };
+   newList[name] = streetAddress;
+    return newList;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, name, streetAddress) {
+    employee[name] = streetAddress
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, name) {
+    var newEmployee = Object.assign({}, employee);
+    delete newEmployee[name];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, name) {
+    delete employee[name];
+    return employee;
+}

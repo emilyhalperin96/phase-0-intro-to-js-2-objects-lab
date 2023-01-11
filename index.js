f370b90ea4c418f4bf641458a0274264bcd9dff8
emilyphalperin@gmail.com <emilyhalperin@Emilys-MacBook-Pro-2.local>
2023-01-11 11:58:50 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+    name: "Emily",
+    streetAddress: "The Pines",
+}
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+     [key]: value,   
+    };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+   employee[key] = value;
+   return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    let newEmployee = {...employee}
+    delete newEmployee[key];
+    return newEmployee;
+}
+    //first we clone the object
+    //then we delete the key
+    //return the new object
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    let destroyedEmployee = employee
+    delete destroyedEmployee[key]
+    return destroyedEmployee;
+}   
